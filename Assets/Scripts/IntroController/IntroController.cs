d3e44202b420bf0d2748f73b95500f9e269ce218
Nguyễn Duy Hoàn <99530243+hoanprd@users.noreply.github.com>
2026-01-13 23:43:08 +0700

    Init submodule and intro fix

--- Assets/Scripts/IntroController/IntroController.cs
+++ Assets/Scripts/IntroController/IntroController.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PRD;
 
 public class IntroController : MonoBehaviour
 {
+    public float timer;
+    IntroFunctional introFunctional = new IntroFunctional();
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitEndIntro());
+        StartCoroutine(introFunctional.RunTimeStart("StartScene", timer));
     }
 
     public void SkipIntro()
     {
-        SceneManager.LoadScene("StartScene");
-    }
-
-    IEnumerator WaitEndIntro()
-    {
-        yield return new WaitForSeconds(90f);
-        SceneManager.LoadScene("StartScene");
+        StopAllCoroutines();
+        introFunctional.SkipIntro("StartScene");
     }
 }
